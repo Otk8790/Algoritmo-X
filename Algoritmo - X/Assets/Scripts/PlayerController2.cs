dbdf865e81d4b321f651f7d65ef01cb043ef3990
Juan Sebastian Murcia Zuñiga <smurcia0820@gmail.com>
2022-03-23 22:55:22 -0500

    Implementacion del inventario

---
+++ Algoritmo - X/Assets/Scripts/PlayerController2.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class PlayerController2 : MonoBehaviour
+{
+    //MovimientoPersonaje
+    private float horizontalMove;
+    private float verticalMove;
+
+    private Vector3 playerInput;
+
+    public float playerSpeed;
+    public float gravity = 9.8f;
+    public float fallVeclocity;
+    public float jumpForce;
+
+    public Camera mainCamera;
+    private Vector3 camForward;
+    private Vector3 camRight;
+    private Vector3 movePlayer;
+
+    public bool isOnSlope = false;
+    private Vector3 hitNormal;
+    public float slideVelocity;
+    public float slopeForceDown;
+    //public AudioSource salto;
+
+    public Animator playerAnimatorController;
+
+    public CharacterController player;
+
+    //Variables animacion
+    //public Animator playerAnimatorController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<CharacterController>();
+        playerAnimatorController = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxis("Horizontal");
+        verticalMove = Input.GetAxis("Vertical");
+
+        playerInput = new Vector3(horizontalMove, 0, verticalMove);
+        playerInput = Vector3.ClampMagnitude(playerInput, 1);
+
+        playerAnimatorController.SetFloat("PlayerWalkVelocity", playerInput.magnitude * playerSpeed);
+
+        camDirection();
+
+        movePlayer = playerInput.x * camRight + playerInput.z * camForward;
+
+        movePlayer = movePlayer * playerSpeed;
+
+        player.transform.LookAt(player.transform.position + movePlayer);
+
+        SetGravity();
+
+        PlayerSkills();
+
+        player.Move(movePlayer * Time.deltaTime);
+    }
+
+    void camDirection()
+    {
+        camForward = mainCamera.transform.forward;
+        camRight = mainCamera.transform.right;
+
+        camForward.y = 0;
+        camRight.y = 0;
+
+        camForward = camForward.normalized;
+        camRight = camRight.normalized;
+    }
+
+    public void PlayerSkills()
+    {
+        if (player.isGrounded && Input.GetButtonDown("Jump"))
+        {
+            //Instantiate(caida);
+            fallVeclocity = jumpForce;
+            movePlayer.y = fallVeclocity;
+            playerAnimatorController.SetTrigger("PlayerJump");
+        }
+    }
+
+    void SetGravity()
+    {
+        if (player.isGrounded)
+        {
+            fallVeclocity = -gravity * Time.deltaTime;
+            movePlayer.y = fallVeclocity;
+        }
+        else
+        {
+            fallVeclocity -= gravity * Time.deltaTime;
+            movePlayer.y = fallVeclocity;
+            playerAnimatorController.SetFloat("PlayerVerticalVelocity", player.velocity.y);
+        }
+
+        playerAnimatorController.SetBool("IsGrounded", player.isGrounded);
+    }
+
+    private void OnAnimatorMove()
+    {
+        
+    }
+}
