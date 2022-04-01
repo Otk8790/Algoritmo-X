7bb1ff3428191eaef3f5150fb5a9d80b55a17b3b
Juan Sebastian Murcia Zuñiga <smurcia0820@gmail.com>
2022-03-31 22:08:24 -0500

    Implementacion de dialogo (Faltan por arreglar cosas)

---
+++ Algoritmo - X/Assets/Scripts/Dialogos/Dialogador.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialogador : MonoBehaviour
+{
+    public int estadoActual = 0;
+    public EstadoDialogo[] estados;
+
+    public GameObject dialogueMark;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogueMark.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            dialogueMark.SetActive(true);
+            if(Input.GetKeyDown(ControlDialogos.singleton.configuracion.teclaInicioDialogo))
+            {
+                StartCoroutine(ControlDialogos.singleton.Decir(estados[estadoActual].frases));
+            }
+        }
+    }
+}
