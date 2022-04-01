1e30f17c19621320fab79e28d2aa064c971476e7
AndresMolina20 <andreskool17@gmail.com>
2022-04-01 16:09:54 -0500

    Ajustes (Plataformas)

--- Algoritmo - X/Assets/Scripts/RotacionPlataformas.cs
+++ Algoritmo - X/Assets/Scripts/RotacionPlataformas.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class RotacionPlataformas : MonoBehaviour
 {
+    public float x;
+    public float y;
+    public float z;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,7 @@ public class RotacionPlataformas : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(new Vector3(x, y, z) * Time.deltaTime);
     }
+
 }
