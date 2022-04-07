04509b0ab065bdce787061882246d33dc71279da
Otk8790 <otk8790@gmail.com>
2022-04-07 09:53:14 -0500

    Nivel 1 Completo

--- Algoritmo - X/Assets/Scripts/plataforma.cs
+++ Algoritmo - X/Assets/Scripts/plataforma.cs
@@ -1,26 +1,111 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+ 
 public class plataforma : MonoBehaviour
 {
-    public Transform[] target;
-    public float speed = 6.0f;
-
-    int curPos = 0;
-    int nextPos = 1;
-
-    private void FixedUpdate()
+ 
+    [SerializeField]
+    float speed;
+ 
+    [SerializeField]
+    Transform startPoint, endPoint;
+ 
+    [SerializeField]
+    float changeDirectionDelay;
+ 
+    
+    private Transform destinationTarget, departTarget;
+ 
+    private float startTime;
+ 
+    private float journeyLength;
+ 
+    bool isWaiting;
+ 
+ 
+    
+    void Start()
     {
-        transform.position = Vector3.MoveTowards(transform.position, target[nextPos].position, speed * Time.deltaTime);
-
-        if (Vector3.Distance(transform.position, target[nextPos].position) <= 0)
+        departTarget = startPoint;
+        destinationTarget = endPoint;
+ 
+        startTime = Time.time;
+        journeyLength = Vector3.Distance(departTarget.position, destinationTarget.position);
+    }
+ 
+   
+    void FixedUpdate()
+    {
+        Move();
+    }
+ 
+    private void Move()
+    {
+ 
+ 
+        if (!isWaiting)
         {
-            curPos = nextPos;
-            nextPos++;
-
-            if (nextPos > target.Length - 1)
-                nextPos = 0;
+            if(Vector3.Distance(transform.position, destinationTarget.position) > 0.01f)
+            {
+                float distCovered = (Time.time - startTime) * speed;
+ 
+                float fractionOfJourney = distCovered / journeyLength;
+ 
+                transform.position = Vector3.Lerp(departTarget.position, destinationTarget.position, fractionOfJourney);
+            }
+            else
+            {
+                isWaiting = true;
+                StartCoroutine(changeDelay());
+            }
+        }
+ 
+ 
+    }
+ 
+    void ChangeDestination()
+    {
+ 
+        if(departTarget == endPoint && destinationTarget == startPoint)
+        {
+            departTarget = startPoint;
+            destinationTarget = endPoint;
+        }
+        else
+        {
+            departTarget = endPoint;
+            destinationTarget = startPoint; 
+        }
+ 
+    }
+    IEnumerator changeDelay()
+    {
+        yield return new WaitForSeconds(changeDirectionDelay);
+        ChangeDestination();
+        startTime = Time.time;
+        journeyLength = Vector3.Distance(departTarget.position, destinationTarget.position);
+        isWaiting = false;
+    }
+ 
+ 
+ 
+   
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.transform.parent = transform;
+ 
         }
     }
+ 
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.transform.parent = null;
+        }
+    }
+ 
 }
